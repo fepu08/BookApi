3a59764ea4c4d957df237e6e5cf7625e937d98a2
fepu08 <ferencbesenyei@protonmail.com>
2021-03-06 15:03:39 +0100

    Many to Many relationships

--- BookApi_Project/Models/Author.cs
+++ BookApi_Project/Models/Author.cs
@@ -11,6 +11,6 @@ namespace BookApi_Project.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public virtual Country Country { get; set; }
-
+        public virtual ICollection<BookAuthor> BookAuthors { get; set; }
     }
 }
