d95d3ec3ace5b13d45e816479d06b63fdc923fb3
fepu08 <ferencbesenyei@protonmail.com>
2021-03-06 17:08:59 +0100

    Add data annotations

--- BookApi_Project/Models/Category.cs
+++ BookApi_Project/Models/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,14 @@ namespace BookApi_Project.Models
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "Category cannot be more than 50 characters")]
         public string Name { get; set; }
+
         public virtual ICollection<BookCategory> BookCategories { get; set; }
     }
 }
--- BookApi_Project/Models/Review.cs
+++ BookApi_Project/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,21 @@ namespace BookApi_Project.Models
 {
     public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Review Headline must be between 3 and 10 characters")]
         public string Headline { get; set; }
+
+        [Required]
+        [StringLength(2000, MinimumLength = 50, ErrorMessage = "Review Text must be between 50 and 2000 characters")]
         public string ReviewText { get; set; }
+
+        [Required]
         public int Rating { get; set; }
+
         public virtual Reviewer Reviewer { get; set; }
         public virtual Book Book { get; set; }
     }
